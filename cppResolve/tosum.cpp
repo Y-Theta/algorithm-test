cf85433e28f00e193665bd2b5c147e001f7b046d
杨杰铭 <yangjm@mctech.vip>
2025-09-22 14:40:30 +0800

    add cpp config

---
+++ cppResolve/tosum.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+
+int main() {
+    std::cout << "sss";
+    int x = 2;
+    int y = 3;
+    cout << endl << y;
+    return y;
+}
