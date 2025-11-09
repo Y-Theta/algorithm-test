c1139e65d55bcbf05955784c3f04bbd676d21921
杨杰铭 <yangjm@mctech.vip>
2025-11-09 12:06:18 +0800

    add resolve

--- pythonResolve/Solution1.py
+++ pythonResolve/Solution1.py
@@ -1638,3 +1638,18 @@ class Solution1:
         return result
 
     # endregion
+
+    # region Solution 2169
+    def countOperations(self, num1: int, num2: int) -> int:
+        if num1 == 0 or num2 == 0:
+            return 0
+        if num1 % num2 == 0:
+            return num1 / num2
+        if num2 % num1 == 0:
+            return num2 / num1
+        if num2 > num1:
+            return self.countOperations(num1, num2 - num1) + 1
+        else:
+            return self.countOperations(num1 - num2, num2) + 1
+
+    # endregion
