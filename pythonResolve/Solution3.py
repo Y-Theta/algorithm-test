8a6479c28cf031110cbf6bfe437603c0b884e981
杨杰铭 <yangjm@mctech.vip>
2026-02-15 19:53:23 +0800

    add resolve

--- pythonResolve/Solution3.py
+++ pythonResolve/Solution3.py
@@ -30,3 +30,46 @@ class Solution3:
         return dp[n - 1]
 
     # endregion
+    
+    # region Solution 67
+    def addBinary(self, a: str, b: str) -> str:
+        la = list(a)
+        la.reverse()
+        lb = list(b)
+        lb.reverse()
+        sresult = []
+        index = 0
+        presum = 0
+        while index < len(la) or index < len(lb):
+            ca = 0
+            if index < len(la):
+                ca = ord(la[index]) - ord('0')
+            cb = 0
+            if index < len(lb):
+                cb = ord(lb[index]) - ord('0')
+            csum = ca + cb + presum
+            presum = csum // 2
+            current = csum % 2
+            sresult.insert(0,str(current))
+            index+=1
+        
+        if presum > 0:
+            sresult.insert(0,'1')
+        
+        return ''.join(sresult)
+        
+    # endregion
+    
+    # region Solution 422
+    def validWordSquare(self, words: List[str]) -> bool:
+        for i in range(len(words)):
+            if len(words) < len(words[i]):
+                return False
+            for j in range(i + 1):
+                if j < len(words[i]):
+                    if len(words[j]) <= i or words[i][j] != words[j][i]:
+                        return False
+                elif len(words[j]) > i:
+                    return False
+        return True
+    # endregion
